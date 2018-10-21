c75f1b26d343884c0cec40f35c5f77f998fce9ef
Sam <srahbar12@hotmail.com>
2018-10-21 16:49:33 -0600

    more examples are added"

--- Examples.sql
+++ Examples.sql
@@ -1,4 +1,4 @@
---  Examples with solutions in SQL Easy to Hard
+l--  Examples with solutions in SQL Easy to Hard
 --  Examples are extracted from Leetcode.com
 
 
@@ -36,3 +36,53 @@ each person in the Person table, regardless if there is an address for each of t
 
 SELECT FirstName, LastName, City, State FROM Person
 LEFT JOIN Address on Person.PersonId = Address.PersonId;
+
+
+/* Question 2: Level of Difficulty >> Intermediate
+
+Write a SQL query to get the second highest salary from the Employee table.
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+
+For example, given the above Employee table, 
+the query should return 200 as the second highest salary.
+If there is no second highest salary, then the query should return null.
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+*/
+
+
+SELECT MAX(salary) AS SecondHighestSalary FROM Employee
+WHERE salary < (SELECT Max(Salary) FROM Employee);
+
+
+
+ 
+/* Question 3: Level of Difficulty >>> Intermediate
+
+Write a SQL query to get the nth highest salary from the Employee table.
+
+For example, given the above Employee table, the nth highest salary where n = 2 is 200. If there is no nth highest salary, then the query should return null.
+
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+
+*/
+
+ 
+
+
