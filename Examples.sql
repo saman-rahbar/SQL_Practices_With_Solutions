0ad507cf9bde53f5af7b5fa11359983a6ab59416
Sam <srahbar12@hotmail.com>
2018-10-21 15:41:12 -0600

    Example file is added

---
+++ Examples.sql
@@ -0,0 +1,38 @@
+/* Examples with solutions in SQL
+Easy to Hard*/
+
+
+
+/* Question 1: 
+
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| PersonId    | int     |
+| FirstName   | varchar |
+| LastName    | varchar |
++-------------+---------+
+PersonId is the primary key column for this table.
+
+
+Table: Address
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| AddressId   | int     |
+| PersonId    | int     |
+| City        | varchar |
+| State       | varchar |
++-------------+---------+
+AddressId is the primary key column for this table.
+
+
+Write a SQL query for a report that provides the following information for
+each person in the Person table, regardless if there is an address for each of those people:*/
+
+
+SELECT FirstName, LastName, City, State FROM Person
+LEFT JOIN Address on Person.PersonId = Address.PersonId;
