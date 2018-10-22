e472af7c320e3e8fac606032630a59c35a065bc2
Sam <srahbar12@hotmail.com>
2018-10-21 22:07:19 -0600

    New examples added

--- Examples.sql
+++ Examples.sql
@@ -126,3 +126,36 @@ Given the Employee table, write a SQL query that finds out employees who earn mo
 
 SELECT Name as Employee FROM Employee E
 WHERE Salary > (SELECT Salary FROM Employee E2 WHERE E.ManagerId = E2.Id);
+
+
+
+
+/* Question 5, Level of difficulty >>> Easy
+
+
+Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+Note: All emails are in lowercase.
+*/
+
+
+
+
+SELECT Email FROM Person
+GROUP BY Email
+HAVING COUNT(Email) >= 2;
+
