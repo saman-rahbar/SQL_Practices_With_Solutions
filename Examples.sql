4b91ac7efd6dadc94302ddd827456b7d113376a4
Sam <srahbar12@hotmail.com>
2018-10-22 03:37:08 -0600

    New exxamples added

--- Examples.sql
+++ Examples.sql
@@ -159,3 +159,42 @@ SELECT Email FROM Person
 GROUP BY Email
 HAVING COUNT(Email) >= 2;
 
+
+/* Question 6. Level of Difficulty >>> Easy
+
+
+Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+
+Table: Customers.
+
++----+-------+
+| Id | Name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Table: Orders.
+
++----+------------+
+| Id | CustomerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Using the above tables as example, return the following:
+
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+*/
+
+SELECT Name as Customers FROM Customers 
+WHERE Id NOT IN (SELECT CustomerId FROM Orders);
+
+
